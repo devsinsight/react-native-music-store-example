923ffcc6455a99ceea77ef8f0a6c5bd823d8c49d
Jose Luis Olivares Rojas <olivares.rojas.jose@gmail.com>
2018-02-17 10:05:41 -0500

    Music Store

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+        <Header title={'Albums'} />
+    );
+
+AppRegistry.registerComponent('MusicStore', () => App);
